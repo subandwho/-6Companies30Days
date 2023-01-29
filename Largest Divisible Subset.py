4e0d124a5bb72eedeb3744f9935b2ba0002b08de
Subandhu <71547473+subandwho@users.noreply.github.com>
2023-01-29 18:06:38 +0530

    Create Largest Divisible Subset.py

---
+++ Largest Divisible Subset.py
@@ -0,0 +1,9 @@
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        if len(nums) == 0: return []
+        nums.sort()
+        sol = [[ele] for ele in nums]
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 and len(sol[i]) < len(sol[j]) + 1:
+                    sol[i] = sol[j] + [nums[i]]
+        return max(sol, key=len)
