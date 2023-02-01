17500ea0cb15bc3e19ccadb6e0b4f3fc046b2508
Subandhu <71547473+subandwho@users.noreply.github.com>
2023-02-02 01:12:35 +0530

    Create Number of Substrings.py

---
+++ Number of Substrings.py
@@ -0,0 +1,6 @@
+    def numberOfSubstrings(self, s: str) -> int:
+        res, last = 0, [-1] * 3
+        for i, c in enumerate(s):
+            last[ord(c) - 97] = i
+            res += 1 + min(last)
+        return res
